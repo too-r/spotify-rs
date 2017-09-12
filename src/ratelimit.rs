05f5fc59a102a8c7bd0aa7993fd16d49f244f964
too-r <william@huddie.net>
2017-09-12 20:02:53 +0000

    Begin structuring a ratelimit module

--- src/ratelimit.rs
+++ src/ratelimit.rs
@@ -1,1 +1,22 @@
+use std::sync::Mutex;
+use std::collections::BTreeMap;
+
+use hyper;
+
+#[derive(Default)]
+struct RateLimit {
+    reset: i64,
+    limit: i64,
+    remaining: i64,
+}
+
+pub struct RateLimits {
+    global: Mutex<RateLimit>,
+    endpoints: Mutex<BTreeMap<String, RateLimit>>,
+}
+
+impl RateLimits {
+    //Issue a premptive check to see whether we are being rate-limited.
+    pub fn pre_check(&self, url: &str)
+}
 
