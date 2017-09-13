f0b8b06da466151abd22eadc5a4f96a21317bf49
too-r <william@huddie.net>
2017-09-13 18:18:40 +0000

    Begin fleshing out an impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 extern crate chrono;
 extern crate hyper;
 extern crate websocket;
@@ -7,21 +6,18 @@ macro_rule! api_concat {
     ($e:expr) => (concat!("https://api.spotify.com/v1", $e))
 }
 
-macro_rules! request {
-    ($self_:ident, $method:ident($body:expr), $url:expr, $($rest:tt)*) => {{
-		let path = format!(api_concat!($url), $($rest)*);
-		try!($self_.request(&path, || $self_.client.$method(&path).body(&$body)))
-	}};
-	($self_:ident, $method:ident, $url:expr, $($rest:tt)*) => {{
-		let path = format!(api_concat!($url), $($rest)*);
-		try!($self_.request(&path, || $self_.client.$method(&path)))
-	}};
-	($self_:ident, $method:ident($body:expr), $url:expr) => {{
-		let path = api_concat!($url);
-		try!($self_.request(path, || $self_.client.$method(path).body(&$body)))
-	}};
-	($self_:ident, $method:ident, $url:expr) => {{
-		let path = api_concat!($url);
-		try!($self_.request(path, || $self_.client.$method(path)))
-	}};
+//Our struct describing Spotify.
+pub struct Spotify {
+    ratelimits: RateLimits,
+    client: hyper::Client,
+}
+
+impl Spotify {
+    //Create a new instance of the struct.
+    pub fn new() -> Self {
+        Spotify {
+            ratelimits: RateLimits::new(),
+            client: hyper::Client::new(),
+        }
+    }
 }
